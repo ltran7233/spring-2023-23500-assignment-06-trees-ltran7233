3a6f97373178f4a4b2b1d9d0407b1fb852543478
ltran7233 <linat@DESKTOP-KO36FGR.localdomain>
2023-05-14 20:39:40 -0400

    added starter files

---
+++ BSTree.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include "Node.h"
+
+class BSTree{
+ private:
+  Node *root;
+  std::string traverse(Node *n);
+
+public:
+  BSTree();
+
+  void setup();
+
+  std::string get_debug_string();
+
+
+
+  int search(int value);
+  void insert(int value);
+
+  int treesum();
+  int treesum(Node *n);
+  int countodds();
+  int countodds(Node *n);
+  int oddsum();
+  int oddsum(Node *n);
+  int searchr(int value); // recursive search
+  int searchr(int value, Node *p);
+
+};
